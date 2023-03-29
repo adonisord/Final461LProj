eb2ceba6db00df1ad994f92be9f5748606302ddb
Suprita Ashok <supritaashok@utexas.edu>
2023-03-29 17:49:25 -0500

    Update initHwSets.py

--- backend/initHwSets.py
+++ backend/initHwSets.py
@@ -1,22 +1,21 @@
 from pymongo import MongoClient
+import configparser
+import csv
 
 client = MongoClient("mongodb+srv://adalordonez:Welcome123!@projectwebsite.o5wvc4k.mongodb.net/test")
 database = client.WebsiteDB
 hwSets = database.HardwareSetCollection
 
 def initializeHardwareSets():
-    initHwSets = [
-        {
-            'set':1,
-            'capacity':100,
-            'availability':100
-        },
-        {
-            'set':2,
-            'capacity':100,
-            'availability':100
-        }
-    ]
+
+    initHwSets = []
+    with open('hwsets.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        i = 0
+        for row in reader:
+            initHwSets[i] = {row['set'], row['capacity'], row['availability']}
+            print(initHwSets[i])
+            i += 0
     hwSets.insert_many(initHwSets)
 
 initializeHardwareSets()
